7ecaac59a55dcea55eff7dd4202f16a31aa07ddd
archanpatkar <pat@archanpatkar.com>
2020-07-18 13:50:48 +0530

    WIP

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from os import sys
 from pprint import pprint
 from collections import namedtuple
 
-### Precedence Climbing LR(1) Math expression parser and evaluator
-empty = [" ","\n","\r","\t"]
+# Math and Logic DSL
+# Parser and Evaluator based on Precedence Climbing LR(1) Algorithm
+# Based on: http://www.engr.mun.ca/~theo/Misc/exp_parsing.htm
+empty = [" ","\r","\t"]
 def isWhite(str):
     if str in empty:
         return True
@@ -29,15 +31,38 @@ def parseNum(str,i,l):
             break
     return (int(buff),i,l)
 
-ops = ["+","-","/","*","(",")","^"]
-opsname = {
+bools = ["T","F"]
+def isBool(str):
+    if str in bools:
+        return True
+    return False
+
+ops = [
+        "+","-","/","*","(",")","^","=","|","&","~","@",
+        "#","==",">","<",">=","<=","~=","[","]","{","}",
+        ",","->",";"
+    ]
+keywords = ["true","false","if","then","else","def","U","B"]
+token_name = {
     "+":"ADD",
     "-":"SUBS",
     "/":"DIV",
     "*":"MUL",
     "(":"LPAREN",
     ")":"RPAREN",
-    "^":"EXP"
+    "^":"EXP",
+    "|":"OR",
+    "&":"AND",
+    "~":"NOT",
+    "=":"ASGN",
+    ">":"GT",
+    "<":"LT",
+    "if":"IF",
+    "then":"THEN",
+    "else":"ELSE",
+    "def":"DEF",
+    ",":"SEP",
+    ";":"SEMI"
 }
 
 Token = namedtuple("Token",["type","val"])
@@ -50,11 +75,19 @@ def tokenize(str):
         if isNumber(c):
             num,i,l = parseNum(str,i,l)
             tokens.append(Token("NUM",num))
+        elif isBool(c):
+            if c == "T":
+                tokens.append(Token("BOOL",True))
+            else:
+                tokens.append(Token("BOOL",False))
         elif c in ops:
-            tokens.append(Token(opsname[c],c))
-        elif isWhite(c):
-            tokens.append(Token("WHITE",c))
+            tokens.append(Token(token_name[c],c))
+        elif c in keywords:
+            tokens.append(Token(token_name[c],c))
+        elif c == "\n" or c == ";":
+            tokens.append(Token("LINEEND",c))
         i+=1
+    # tokens.append(Token("EOF",""))
     return tokens
 
 def BinOp(op,left,right):
@@ -63,16 +96,21 @@ def BinOp(op,left,right):
 def UnOp(op,value):
     return { "type":"Unary", "op":op, "right":value, "reduced":False }
 
-binaryops = ["ADD","SUBS","DIV","MUL","EXP"]
-unaryops = ["SUBS"]
+binaryops = ["ADD","SUBS","DIV","MUL","EXP","OR","AND","LT","GT"]
+unaryops = ["SUBS","ADD","NOT"]
 opconfig = {
-    "ADD":(1,1),
-    "SUBS":(1,1),
-    "NEG":(2,0),
-    "POS":(2,0),
-    "MUL":(3,1),
-    "DIV":(3,1),
-    "EXP":(4,0)
+    "OR":(1,1),
+    "AND":(2,1),
+    "LT":(3,1),
+    "GT":(3,1),
+    "ADD":(4,1),
+    "SUBS":(4,1),
+    "MUL":(5,1),
+    "DIV":(5,1),
+    "EXP":(6,0),
+    "NEG":(7,0),
+    "POS":(7,0),
+    "NOT":(7,0)
 }
 
 swap = {
@@ -96,13 +134,13 @@ def parse(tokens):
         out = exp(0,tokens)
         if isinstance(out,dict):
             exps.append(out)
-    # assert getNext(tokens) == None
     return exps
 
 def exp(min,tokens):
+    print(tokens)
     lhs = term(tokens)
     lookahead = peekNext(tokens)
-    while (lhs.type == "WHITE" and lhs.val != "\n") and (lookahead != None) and (lookahead.type in binaryops) and (opconfig[lookahead.type][0] >= min):
+    while (lookahead != None) and (lookahead.val != "\n") and (lookahead.type in binaryops) and (opconfig[lookahead.type][0] >= min):
         op = getNext(tokens)
         n = opconfig[op.type][0]
         if opconfig[op.type][1] == 1:
@@ -130,6 +168,9 @@ def term(tokens):
     elif current.type == "NUM":
         getNext(tokens)
         return current.val
+    elif current.type == "BOOL":
+        getNext(tokens)
+        return current.val
     elif current.type == "WHITE":
         return current
     else:
@@ -143,15 +184,46 @@ opmap = {
     "DIV": lambda x,y: x / y,
     "NEG": lambda x: -x,
     "POS": lambda x: +x,
+    "NOT": lambda x: not(x),
+    "AND": lambda x,y: x and y,
+    "OR": lambda x,y: x or y,
+    "LT": lambda x,y: x < y,
+    "GT": lambda x,y: x > y,
     "EXP": math.pow
 }
+
+bool_ops = ["NOT","AND","OR"]
+num_ops = ["ADD","SUBS","DIV","MUL","EXP","NEG","POS"]
+
+def coerse(op,*params):
+    temp = []
+    if op in bool_ops:
+        for p in params:
+            if isinstance(p,int) or isinstance(p,float):
+                if p > 0:
+                    temp.append(True)
+                elif p == 0:
+                    temp.append(False)
+            else:
+                temp.append(p)
+    elif op in num_ops:
+        for p in params:
+            if isinstance(p,bool):
+                if p:
+                    temp.append(1)
+                else:
+                    temp.append(0)
+            else:
+                temp.append(p)
+    return temp
 
 def eval(ast):
     if isinstance(ast,list):
         outcome = []
         for exp in ast:
             outcome.append(eval(exp))
-    if isinstance(ast,dict):
+        return outcome
+    elif isinstance(ast,dict):
         if ast["reduced"]:
             return ast
         if ast["type"] == "Binary":
@@ -166,8 +238,14 @@ def eval(ast):
     else:
         return ast
 
-tokens = tokenize("(-500)*500+6")
+tokens = tokenize("(T | F) & ~F & 5 > 5")
 print(tokens)
 ast = parse(tokens)
 pprint(ast,indent=4)
 print(eval(ast))
+
+# tokens = tokenize("(-500)*500+6")
+# print(tokens)
+# ast = parse(tokens)
+# pprint(ast,indent=4)
+# print(eval(ast))

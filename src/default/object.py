eac3635c6c3de14ff618291666236b806a3c9a64
archanpatkar <pat@archanpatkar.com>
2020-08-03 23:42:19 +0530

    updates

--- src/default/object.py
+++ src/default/object.py
@@ -1,2 +1,12 @@
 # Start defining the Object system 
-# The object system should in it itself be complete and serielizable to JSON
+# The object system should in it itself be complete and serializable to JSON
+
+class Object(dict):
+    def __init__(self):
+        self.writable = True
+
+    def get(self,key):
+        pass
+
+    def set(self,key):
+        pass

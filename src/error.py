19e2bc837509a127543bc5caf403ae8f32f619a5
Archan Patkar <pat@archanpatkar.com>
2020-09-23 23:22:11 +0530

    updates

--- src/error.py
+++ src/error.py
@@ -1,7 +1,18 @@
 import sys
 
-def lexing_error(token):
-    print("Unexpected character -> {}".format(token))
+# Printing with topological location
+def topo_loc(line,lineno,colno,msg=False):
+    buff = ""
+    buff += "{}| {}\n".format(lineno,line)
+    pad = len(str(lineno))+colno+2
+    buff += "^\n".rjust(pad)
+    if msg:
+        buff += msg + "\n"
+    buff += "colno {}, lineno {}\n".format(colno,lineno)
+    return buff
+
+def lexing_error(msg):
+    print(msg)
     sys.exit("Lexing Error")
 
 def parse_error(msg):
